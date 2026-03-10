10de9af8ce93499aac5b911a08e3e31c0df6c452
ngbautista01-lab <ngbautista01@gmail.com>
2026-03-09 20:17:31 -0400

    feat: Add account context and port for manage the balance in the transactions

--- src/modules/accounts/accounts.module.ts
+++ src/modules/accounts/accounts.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountBalanceContextService } from './application/account-balance-context.service';
 import { AccountService } from './application/account.service';
+import { ACCOUNT_BALANCE_CONTEXT_PORT } from './application/ports/account-balance-context.port';
 import { ACCOUNT_REPOSITORY } from './application/ports/account.repository';
 import { AccountEntity } from './domain/account.entity';
 import { TypeOrmAccountRepository } from './infrastructure/typeorm-account.repository';
@@ -9,14 +11,19 @@ import { AccountResolver } from './presentation/account.resolver';
 @Module({
   imports: [TypeOrmModule.forFeature([AccountEntity])],
   providers: [
+    AccountBalanceContextService,
     AccountService,
     AccountResolver,
     TypeOrmAccountRepository,
+    {
+      provide: ACCOUNT_BALANCE_CONTEXT_PORT,
+      useExisting: AccountBalanceContextService,
+    },
     {
       provide: ACCOUNT_REPOSITORY,
       useExisting: TypeOrmAccountRepository,
     },
   ],
-  exports: [AccountService, TypeOrmModule],
+  exports: [AccountService, ACCOUNT_BALANCE_CONTEXT_PORT, TypeOrmModule],
 })
 export class AccountsModule {}
